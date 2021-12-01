2c7e1b6302b01dae8e71e312616570bf9caba694
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-01 15:12:13 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,1 +1,12 @@
+btn = document.getElementById('btn');
+// main2 = document.getElementById('main2');
+content = document.querySelector('.content')
+btn2 = document.getElementById('btn2');
 
+btn.addEventListener('click',function () {
+    content.classList.add("content3")
+})
+
+btn2.addEventListener('click',function () {
+    content.classList.remove("content3")
+})
